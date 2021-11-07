6a55b3f35a6428b8962b0460e25e90c4e4e1c073
Ilia <iilyazakos@gmail.com>
2021-11-07 21:55:23 +0300

    add todo upgrade

--- Games_dashboard.py
+++ Games_dashboard.py
@@ -22,19 +22,22 @@ games = _game[['Rank', 'Name', 'Platform', 'Year',
                'Genre', 'Publisher', 'NA_Sales', 'EU_Sales',
                'JP_Sales', 'Other_Sales', 'Global_Sales']].replace([np.nan, np.inf], 0)
 
-# Удаляем нулевые значения по столбцу 'Year', делаем его целочисленным для удобства
-# Удаляем нулевые значения по столбцу 'Publisher'
+games[['NA_Sales', 'EU_Sales',
+               'JP_Sales', 'Other_Sales', 'Global_Sales']] = games[['NA_Sales', 'EU_Sales',
+               'JP_Sales', 'Other_Sales', 'Global_Sales']] * 1000000
 
 st.set_page_config(layout = "wide")
 st.title('Game sales dashboard')
-st.write('bla bla bla')
+st.write('')# TODO: write a description
+
 
 with st.container() as row_description:
-    st.header("Hello it's my github")
-    github = st.button(label = 'Github')
+    github = st.button(label = 'My Github')
 #open my github
 if github: open_link("https://github.com/iilyazakos")
 
+# Удаляем нулевые значения по столбцу 'Year', делаем его целочисленным для удобства
+# Удаляем нулевые значения по столбцу 'Publisher'
 games.drop(games[games['Year'] == 0.0].index, inplace = True)
 games.drop(games[games['Publisher'] == 0].index, inplace = True)
 games['Year'] = games['Year'].astype('int64')
@@ -43,28 +46,22 @@ games['Year'] = games['Year'].astype('int64')
 
 #__________________________________________________________
 # Метрики
-# st.title('Average number of sales in North America, Europe, Japan, all and other in millions')
 with st.container() as row_game_total:
-    st.markdown('Average number of sales in North America, Europe, Japan, all and other in millions')
-    col_NA_sales, col_EU_sales, col_JP_sales, col_other_sales, col_global_sales = st.columns(5)
-    with col_NA_sales:
-        st.metric(label = 'North America', value = round(games['NA_Sales'].mean(), 2))
-    with col_EU_sales:
-        st.metric(label = 'Europe', value = round(games['EU_Sales'].mean(), 2))
-    with col_JP_sales:
-        st.metric(label='Japan', value=round(games['JP_Sales'].mean(), 2))
-    with col_other_sales:
-        st.metric(label='Other', value=round(games['Other_Sales'].mean(), 2))
-    with col_global_sales:
-        st.metric(label='Global', value=round(games['Global_Sales'].mean(), 2))
+    col_top_sales_region, col_top_platform, col_top_publisher, col_top_genre = st.columns(4)
+    with col_top_sales_region:
+        st.metric(label = 'Region with the highest sales', value = games[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']].sum().sort_values(ascending = False).index[0].replace('_Sales', ''))
+    with col_top_platform:
+        st.metric(label = 'Platform with the biggest sales', value = games.groupby('Platform')['Global_Sales'].sum().sort_values(ascending = False).index[0])
+    with col_top_publisher:
+        st.metric(label='Publisher with the biggest sales', value = games.groupby('Publisher')['Global_Sales'].sum().sort_values(ascending = False).index[0])
+    with col_top_genre:
+        st.metric(label='Category with the biggest sales', value = games.groupby('Genre')['Global_Sales'].sum().sort_values(ascending = False).index[0])
 
 # _________________________________________________________
 # В какой год выходило больше всего игр
-
 with st.container() as row_game_year:
     col_year_max, col_game_platform = st.columns(2)
     with col_year_max:
-
         game_year_max = games[['Year', 'Name']]
         game_year_max = (game_year_max.groupby(['Year'])['Name'].count()).reset_index()
         game_year_max.drop(game_year_max[game_year_max['Year'] == 2020].index, inplace = True)
@@ -83,31 +80,31 @@ with st.container() as row_game_year:
         st.plotly_chart(px.bar(game_platform, x = 'Platform', y = 'Number of games',
                                title = 'Games on platforms'), use_container_width = True)
 
-# _______________
+# _________________________________
 # Продажи на платформах по годам
-dynamics_Global_Sales_year = games[['Platform', 'Year', 'Global_Sales']]
-select_platform = st.multiselect('select platforms', games['Platform'].unique(), 'GB')
-data_for_fig = pd.DataFrame({'platforms': select_platform})
-dynamics_Global_Sales_year = dynamics_Global_Sales_year[dynamics_Global_Sales_year['Platform'].isin(select_platform)]
-dynamics_Global_Sales_year = (dynamics_Global_Sales_year.groupby(['Year', 'Platform'])['Global_Sales'].sum()).reset_index()
-dynamics_Global_Sales_year.columns = ['Years', 'Platform', 'Global Sales in millions of dollars']
+with st.container() as col_dynamics_Global_Sales:
+    dynamics_Global_Sales_year = games[['Platform', 'Year', 'Global_Sales']]
+    select_platform = st.multiselect('Select platforms', games['Platform'].unique(), 'GB')
+    data_for_fig = pd.DataFrame({'platforms': select_platform})
+    dynamics_Global_Sales_year = dynamics_Global_Sales_year[dynamics_Global_Sales_year['Platform'].isin(select_platform)]
+    dynamics_Global_Sales_year = (dynamics_Global_Sales_year.groupby(['Year', 'Platform'])['Global_Sales'].sum()).reset_index()
+    dynamics_Global_Sales_year.columns = ['Years', 'Platform', 'Global Sales']
 
-st.plotly_chart(px.bar(dynamics_Global_Sales_year, x = 'Years', y = 'Global Sales in millions of dollars', color = 'Platform', barmode='overlay'), use_container_width = True)
+    st.plotly_chart(px.bar(dynamics_Global_Sales_year, x = 'Years', y = 'Global Sales', color = 'Platform', barmode='overlay'), use_container_width = True)
 # _________________________________________________________
 # У каких Publisher самые большие продожи
 with st.container() as row_game_platform:
     game_max_public = games[['Publisher', 'Global_Sales']]
     game_max_public = (game_max_public.groupby(['Publisher'])['Global_Sales'].count()).reset_index()
-    game_max_public.columns = ['Publisher', 'Global Sales in millions of dollars']
+    game_max_public.columns = ['Publisher', 'Global Sales']
 
-    st.plotly_chart(px.line(game_max_public, x = 'Global Sales in millions of dollars', y = 'Publisher',
+    st.plotly_chart(px.line(game_max_public, x = 'Global Sales', y = 'Publisher',
                             title = 'Which Publisher has the biggest sales'), use_container_width = True)
 
 # _________________________________________________________
 # В каком жанре выпускалось больше всего игр
 with st.container() as row_genre_max_produced:
     col_genre_max_produced, col_different_sales = st.columns(2)
-
     with col_genre_max_produced:
         game_genre_max_produced = games[['Name', 'Genre']]
         game_genre_max_produced = (game_genre_max_produced.groupby(['Genre'])['Name'].count()).reset_index()
@@ -122,24 +119,24 @@ with st.container() as row_genre_max_produced:
         data_different_sales = games[['Genre', 'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales']]
         data_different_sales = (data_different_sales.groupby(['Genre'])[
                                     'NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales', 'Global_Sales'].sum()).reset_index()
-        data_different_sales.columns = ['Genre', 'Sales in North America in millions', 'Sales in Europe in millions',
-                                        'Sales in Japan in millions', 'Other sales in millions',
-                                        'All sales worldwide in millions']
+        data_different_sales.columns = ['Genre', 'Sales in North America', 'Sales in Europe',
+                                        'Sales in Japan', 'Other sales',
+                                        'All sales worldwide']
         type_of_sales = st.radio('Select sales region',
-                                 ('Sales in North America in millions', 'Sales in Europe in millions',
-                                  'Sales in Japan in millions', 'Other sales in millions',
-                                  'All sales worldwide in millions'))
-        if type_of_sales == 'Sales in North America in millions': st.plotly_chart(
-            px.bar(data_different_sales, x='Genre', y='Sales in North America in millions', title = 'Sales in different categories'), use_container_width=True)
+                                 ('Sales in North America', 'Sales in Europe',
+                                  'Sales in Japan', 'Other sales',
+                                  'All sales worldwide'))
+        if type_of_sales == 'Sales in North America': st.plotly_chart(
+            px.bar(data_different_sales, x='Genre', y='Sales in North America', title = 'Sales in different categories'), use_container_width=True)
 
-        if type_of_sales == 'Sales in Europe in millions': st.plotly_chart(
-            px.bar(data_different_sales, x='Genre', y='Sales in Europe in millions', title = 'Sales in different categories'), use_container_width=True)
+        if type_of_sales == 'Sales in Europe': st.plotly_chart(
+            px.bar(data_different_sales, x='Genre', y='Sales in Europe', title = 'Sales in different categories'), use_container_width=True)
 
-        if type_of_sales == 'Sales in Japan in millions': st.plotly_chart(
-            px.bar(data_different_sales, x='Genre', y='Sales in Japan in millions', title = 'Sales in different categories'), use_container_width=True)
+        if type_of_sales == 'Sales in Japan': st.plotly_chart(
+            px.bar(data_different_sales, x='Genre', y='Sales in Japan', title = 'Sales in different categories'), use_container_width=True)
 
-        if type_of_sales == 'Other sales in millions': st.plotly_chart(
-            px.bar(data_different_sales, x='Genre', y='Other sales in millions', title = 'Sales in different categories'), use_container_width=True)
+        if type_of_sales == 'Other sales': st.plotly_chart(
+            px.bar(data_different_sales, x='Genre', y='Other sales', title = 'Sales in different categories'), use_container_width=True)
 
-        if type_of_sales == 'All sales worldwide in millions': st.plotly_chart(
-            px.bar(data_different_sales, x='Genre', y='All sales worldwide in millions', title = 'Sales in different categories'), use_container_width=True)
+        if type_of_sales == 'All sales worldwide': st.plotly_chart(
+            px.bar(data_different_sales, x='Genre', y='All sales worldwide', title = 'Sales in different categories'), use_container_width=True)
